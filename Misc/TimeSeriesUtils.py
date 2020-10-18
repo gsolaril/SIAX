61d5a4146c857e42b65cf7deef5e21c7eb84179f
Pablo Narducci <pnarducci@cosmolex.com>
2020-10-18 11:13:45 -0300

    Agregado de funciones para testear el modelo

--- Misc/TimeSeriesUtils.py
+++ Misc/TimeSeriesUtils.py
@@ -9,6 +9,7 @@ import csv
 
 class TimeSeriesUtils:
 
+    @staticmethod
     def get_time_series_from_file(filename = 'MELI(5m).csv', data_idx = 2, time_step_idx = None):
       """
         Lee una serie de tiempo desde un archivo csv
@@ -16,7 +17,7 @@ class TimeSeriesUtils:
         Keyword arguments:
         filename -- nombre del archivo csv
         data_idx -- columna en la cual está la serie de tiempo
-        time_step_idx -- columna en la cual está el tiempo
+        time_step_idx -- columna en la cual está el tiempo. Si es None usa un timestep secuencial (1,2,3,...)
       """
 
       time_step = []
@@ -44,6 +45,7 @@ class TimeSeriesUtils:
       return time, series
 
 
+    @staticmethod
     def plot_series(time_series_1, time_series_2 = None, start=0, end=None, figsize=(30, 6), format="-"):
       """
         Imprime un gráfico de una serie de tiempo.
@@ -73,31 +75,19 @@ class TimeSeriesUtils:
         plt.plot(time_2[start:end], series_2[start:end], format)
       
       plt.show()
-    
-
-    def get_days_to_request(interval):
-      """
-        Yahoo sólo permite descargar información de los últimos días
-        y la cantidad de días hacia atrás que permite depende de la frecuencia
-        
-        Keyword arguments:
-        interval -- intervalo entre datapoints
-      """
-
-      return {
-            '1d': 365,
-            '5m': 59,
-            '1m': 7
-        }[interval]
 
 
-    def get_from_yahoo(instrument = "AMZN", interval = "5m"):
+    @staticmethod
+    def get_time_series_from_yahoo(instrument = "AMZN", interval = "5m", column = "Close"):
       """
         Pide a Yahoo los datos del instrumento pasado por parámetro con la frecuencia indicada
+        y devuelve una time series según la columna indicada.
 
         Keyword arguments:
         instrument -- instrumento para el cual se quiere predecir
-        interval -- intervalo entre datapoints (default 5m)
+        interval -- intervalo entre datapoints (1d, 5m, o 1m). Default: 5m.
+        column -- Qué dato de la vela tomar. Por defecto es Close.
+                  La serie de tiempo se va a basar en estos datos.
       """
 
       days_to_request = {
@@ -122,11 +112,12 @@ class TimeSeriesUtils:
       df = df.reset_index()
 
       # Guardamos el resultado en un csv para no tener que volver a pedirlo
-      df.to_csv("/tmp/" + instrument + "(" + interval + ").csv")
+      df.to_csv("./" + instrument + "(" + interval + ")_yahoo.csv")
 
-      return df
+      return get_time_series_from_dataframe(df, column)
 
 
+    @staticmethod
     def get_time_series_from_dataframe(dataframe, series_column = "Close", time_column = None):
       """
         A partir de un dataframe, y los nombres de las columnas devuelve dos np.arrays
@@ -149,6 +140,7 @@ class TimeSeriesUtils:
       return time, series
 
 
+    @staticmethod
     def split_dataset(time, series, split_time = None):
       """
         Divide el tiempo y la serie en train y test
